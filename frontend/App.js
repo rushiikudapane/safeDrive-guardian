ea647c4fdebbc9461e6309db95d6d44d165625f3
omsakhare1811 <156443119+omakhare1811@users.noreply.github.com>
2024-01-19 16:54:08 +0530

    all componenets created

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './src/Screens/Home';
+import Map from './src/Screens/Map';
+import MyProfile from './src/Screens/MyProfile';
+import Notifications from './src/Screens/Notifications';
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const Stack = createNativeStackNavigator();
+  
+  return( 
+    <NavigationContainer>
+      <Stack.Navigator initialRounteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen 
+          name="Notifications" 
+          component={Notifications}
+        />
+        <Stack.Screen 
+          name="Map" 
+          component={Map}
+          
+          />
+        <Stack.Screen 
+          name="Myprofile" 
+          component={MyProfile}
+          />
+          
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
